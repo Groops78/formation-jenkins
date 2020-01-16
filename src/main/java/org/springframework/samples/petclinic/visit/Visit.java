ef0757289f82a78cfc9d9284d4182981e9307620
Groops78 <59964527+Groops78@users.noreply.github.com>
2020-01-16 17:41:11 +0100

    Update Visit.java

--- src/main/java/org/springframework/samples/petclinic/visit/Visit.java
+++ src/main/java/org/springframework/samples/petclinic/visit/Visit.java
@@ -24,7 +24,9 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.hibernate.validator.constraints.NotEmpty;
+
 import org.springframework.format.annotation.DateTimeFormat;
+
 import org.springframework.samples.petclinic.model.BaseEntity;
 
 /**
